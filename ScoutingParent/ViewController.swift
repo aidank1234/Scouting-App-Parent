f88c61ca67a57af60ba57629ab4329c606c6ac17
Aidan Kaiser <aidankaiser@Aidans-MacBook-Pro.local>
2017-01-25 21:54:48 -0600

    Browse button, text view displaying incoming data

--- ScoutingParent/ViewController.swift
+++ ScoutingParent/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
 
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+    let serviceType = "SCOUTING-APP"
+
+    @IBOutlet weak var textView: UITextView!
+    
+    func updateTextView(text: String) {
+        textView.text = textView.text + text + "\n"
+    }
+    
+    @IBAction func browseButton(_ sender: Any) {
+        // Show the browser view controller
+        self.present(self.browser, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: MCEncryptionPreference.none)
+        self.session.delegate = self
+        
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+                                               session:self.session)
+        
+        self.browser.delegate = self;
+        
+        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
+                                               discoveryInfo:nil, session:self.session)
+        self.assistant.start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +50,58 @@ class ViewController: UIViewController {
     }
 
 
+    func browserViewControllerDidFinish(
+        _ browserViewController: MCBrowserViewController)  {
+        // Called when the browser view controller is dismissed (ie the Done
+        // button was tapped)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        _ browserViewController: MCBrowserViewController)  {
+        // Called when the browser view controller is cancelled
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data,
+                 fromPeer peerID: MCPeerID)  {
+        // Called when a peer sends an NSData to us
+        
+        DispatchQueue.main.async {
+            
+            let recievedString = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue)
+            self.updateTextView(text: recievedString as! String)
+        }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(_ session: MCSession,
+                 didStartReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID, with progress: Progress)  {
+        
+        // Called when a peer starts sending a file to us
+    }
+    
+    func session(_ session: MCSession,
+                 didFinishReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID,
+                 at localURL: URL, withError error: Error?)  {
+        // Called when a file has finished transferring from another peer
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream,
+                 withName streamName: String, fromPeer peerID: MCPeerID)  {
+        // Called when a peer establishes a stream with us
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID,
+                 didChange state: MCSessionState)  {
+        // Called when a connected peer changes state (for example, goes offline)
+        
+        
+    }
 }
 
